cfd04c9e8dd4a0fda25d88c4ef7d7dc53d948c22
TAIYOSAMA <taiyo180601@gmail.com>
2025-12-11 15:34:26 +0900

    refactor: q10.c

--- src/q10.c
+++ src/q10.c
@@ -2,10 +2,8 @@
 #include <stdlib.h>
 
 int factorial (int n) {
-    if (n == 1) 
-        return 1;
-    else
-        return factorial(n - 1) * n;
+    if (n == 1) return 1;
+    return factorial(n - 1) * n;
 }
 
 int choose (int n, int r) {
