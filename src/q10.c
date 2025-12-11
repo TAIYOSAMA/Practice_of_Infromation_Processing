e5afb03cb39511c35c8c6f2588021a4b9574c61d
TAIYOSAMA <taiyo180601@gmail.com>
2025-12-11 15:14:06 +0900

    add: q10.c

---
+++ src/q10.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int factorial (int n) {
+    if (n == 1) 
+        return 1;
+    else
+        return factorial(n - 1) * n;
+}
+
+int combination (int n, int r) {
+    if (n < r) exit(1);
+    return factorial(n) / (factorial(r) * factorial(n - r));
+}
+
+int main () {
+    int n, r;
+
+    printf("n=");
+    scanf("%d", &n);
+    printf("r=");
+    scanf("%d", &r);
+
+    printf("ans=%d\n", combination(n, r));
+
+    return 0;
+}
